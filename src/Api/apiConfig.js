6465f5572b2f0cbbcb2f741173cbd621a823cc6e
marc-codes <adzaibismark@gmail.com>
2024-10-05 13:50:18 +0000

    Api configuration

---
+++ src/Api/apiConfig.js
@@ -0,0 +1,9 @@
+const apiConfig =  {
+
+    baseUrl : 'https://api.themoviedb.org/3/',
+    apiKey : '8daab53e806e1a389e43ee17bc9cdff8',
+    originalImage : (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
+    w500image : (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
+
+}
+export default apiConfig;
--- src/components/common/GlowingSearchInput.jsx
+++ src/components/common/GlowingSearchInput.jsx
@@ -16,7 +16,7 @@ const GlowingSearchInput = () => {
   </div>
 
 
-
+ 
 
   )
 }
