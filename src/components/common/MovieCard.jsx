9d42c159fcd6f20726dfec7ca4e9a7372aeea335
marc-codes <adzaibismark@gmail.com>
2024-10-08 21:54:25 +0000

    removed console.logs and added ratings feature in moviecard.jsx

--- src/components/common/MovieCard.jsx
+++ src/components/common/MovieCard.jsx
@@ -1,36 +1,23 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import apiConfig from "../../Api/apiConfig";
 
 const MovieCard = ({ movie }) => {
-  if (!movie) {
-    console.error("MovieCard received undefined movie prop");
-    return null;
-  }
-
-  let posterUrl;
-  try {
-    // Note the lowercase 'i' in w500image
-    posterUrl = apiConfig.w500image(movie.poster_path || movie.backdrop_path);
-  } catch (error) {
-    console.error("Error generating poster URL:", error);
-    posterUrl = ""; // Fallback to empty string if there's an error
-  }
-
-  console.log(
-    "Rendering MovieCard for:",
-    movie.title || movie.name,
-    "Poster URL:",
-    posterUrl
-  );
+  let posterUrl = apiConfig.w500image(movie.poster_path || movie.backdrop_path);
+  let rating = movie.vote_average ? movie.vote_average.toFixed(1) : "N/A";
 
   return (
     <Link to={`/movie/${movie.id}`}>
-      <div
-        className="movie-card"
-        style={{ backgroundImage: `url(${posterUrl})` }}>
-        <div className="h-40">
-          <h3>{movie.title || movie.name}</h3>
+      <div className="p-2 bg-gray-800 rounded-lg hover:bg-gray-700 transition-colors duration-300">
+        <div className="mt-2 text-center">
+          <img
+            src={posterUrl}
+            alt={movie.title || movie.name}
+            className="w-full h-72 object-cover rounded-md"
+          />
+          <h3 className="text-lg font-semibold text-white">
+            {movie.title || movie.name}
+          </h3>
+          <p className="text-sm text-gray-400 mt-3">Rating: {rating}</p>
         </div>
       </div>
     </Link>
