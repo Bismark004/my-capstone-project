5f2837eb18fa61f3e43f13ea15a26729b1f8025f
marc-codes <adzaibismark@gmail.com>
2024-10-12 19:44:53 +0000

    commit

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,14 +1,19 @@
 import Logo from "./Logo";
+import { Link } from "react-router-dom";
 import FavouriteIcon from "./FavouriteIcon";
-import WatchLater from "./WatchLaterIcon";
+import WatchLaterIcon from "./WatchLaterIcon";
 
 const Header = () => {
   return (
     <div className="w-full bg-blue-900 flex justify-between items-center h-20 p-4">
       <Logo />
-      <div>
-        <FavouriteIcon />
-        <WatchLater />
+      <div className="flex gap-6">
+        <Link to="/favorites">
+          <FavouriteIcon />
+        </Link>
+        <Link to="/watchlater">
+          <WatchLaterIcon />
+        </Link>
       </div>
     </div>
   );
