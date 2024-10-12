b1fe5a11af45012b17fe3f190b4f795e7c3818a1
marc-codes <adzaibismark@gmail.com>
2024-10-12 13:36:26 +0000

    updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./components/pages/Homepage";
 import MovieDetails from "./components/pages/MovieDetails";
+import SearchResults from "./components/common/SearchResults";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="movie/:id" element={<MovieDetails />} />
         <Route path="/tv/:id" element={<MovieDetails />} />
+        <Route path="/search" element={<SearchResults />} />
       </Routes>
     </Router>
   );
