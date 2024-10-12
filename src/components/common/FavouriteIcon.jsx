9ce3702eef793cdecdae6307a556b6640044c001
marc-codes <adzaibismark@gmail.com>
2024-10-12 21:42:56 +0000

    commit

--- src/components/common/FavouriteIcon.jsx
+++ src/components/common/FavouriteIcon.jsx
@@ -1,8 +1,23 @@
-import FavoriteIcon from '@mui/icons-material/FavoriteOutlined';
+import { useState } from "react";
+import FavoriteIcon from "@mui/icons-material/FavoriteOutlined";
+import Tooltip from "@mui/material/Tooltip";
 
 const FavouriteIcon = () => {
-    return (
-    <FavoriteIcon style={{ color: 'white' }} />
-);
-}
+  const [hover, setHover] = useState(false);
+
+  return (
+    <Tooltip title="Favourite" arrow>
+      <FavoriteIcon
+        style={{
+          color: hover ? "yellow" : "white",
+          cursor: "pointer",
+          transition: "color 0.3s ease", // Smooth transition
+        }}
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+      />
+    </Tooltip>
+  );
+};
+
 export default FavouriteIcon;
