e64a65d76ea1b70cabbaf7846207aa2ed3f7fb48
marc-codes <adzaibismark@gmail.com>
2024-10-14 20:58:54 +0000

    commit

--- src/Api/apiConfig.js
+++ src/Api/apiConfig.js
@@ -1,20 +1,10 @@
 const apiConfig = {
-    baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: '6601fa15c9052bc1fd6508661d601719',
-    originalImage: (imgPath) => {
-      if (!imgPath) {
-        console.error('Invalid image path for originalImage');
-        return '';
-      }
-      return `https://image.tmdb.org/t/p/original/${imgPath}`;
-    },
-    w500image: (imgPath) => {
-      if (!imgPath) {
-        console.error('Invalid image path for w500image');
-        return '';
-      }
-      return `https://image.tmdb.org/t/p/w500/${imgPath}`;
-    }
-  };
-  
-  export default apiConfig;
+  baseUrl: "https://api.themoviedb.org/3/",
+  apiKey: import.meta.env.VITE_TMDB_API_KEY,
+  originalImage: (imgPath) =>
+    imgPath ? `https://image.tmdb.org/t/p/original/${imgPath}` : "",
+  w500image: (imgPath) =>
+    imgPath ? `https://image.tmdb.org/t/p/w500/${imgPath}` : "",
+};
+
+export default apiConfig;
